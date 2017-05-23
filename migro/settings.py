1d1fa86cdffbd511934ed802ed535d339265356c
andrewshkovskii <andrewshkovskii@gmail.com>
2017-05-23 19:48:33 +0700

    Remove `public_key` value from settings

--- migro/settings.py
+++ migro/settings.py
@@ -10,13 +10,13 @@
 UPLOAD_BASE = 'https://upload.uploadcare.com/'
 
 # Project public key.
-PUBLIC_KEY = '9a598e2a47fe961ea412'
+PUBLIC_KEY = None
 
 FROM_URL_TIMEOUT = 10
 
 # Maximum number of concurrent upload requests
 # Since we use `from_url` feature this will be just GET requests.
-MAX_CONCURRENT_UPLOADS = 1
+MAX_CONCURRENT_UPLOADS = 20
 
 # Maximum number of concurrent jobs to wait of status update of uploaded file.
 MAX_CONCURRENT_CHECKS = 20
