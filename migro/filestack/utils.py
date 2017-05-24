e3149a695324889441cf90f59d749ce715416e1c
andrewshkovskii <andrewshkovskii@gmail.com>
2017-05-24 15:03:34 +0700

    Add filestack url builder

---
+++ migro/filestack/utils.py
@@ -0,0 +1,16 @@
+"""
+    migro.filestack.utils
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Utils and helpers for filestack migration.
+
+"""
+
+
+def build_url(handle_or_url):
+    """Builds URL for filestack-hosted file based on file handler or, 
+    if it is already URL - return it."""
+    if handle_or_url.startswith('https://'):
+        return handle_or_url
+    else:
+        return 'https://cdn.filestackcontent.com/{0}'.format(handle_or_url)
