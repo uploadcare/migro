bba9d73913ecc2aac9b86368606617893f95132b
andrewshkovskii <andrewshkovskii@gmail.com>
2017-07-21 14:05:46 +0400

    Refactor filestack dev console script:
    - Add progress notifications
    - Add proper next page handling
    - Add filename and extension to the result file

--- migro/filestack/dev_console.js
+++ migro/filestack/dev_console.js
@@ -12,19 +12,28 @@
 
   var loadMoreCallback = function (event, jqXHR, options, data) {
     if (options.url.startsWith(window.location.pathname + '/')) {
-      if (data) {
+      if ($btnLoadMore.attr('disabled') !== 'disabled'){
+        console.log('Downloading more data for files list...');
         $btnLoadMore.trigger('click', true);
       } else {
+        console.log('Done collect your files links!');
+        console.log('Preparing file for download...');
         $('a.js_download').each(function (index, item) {
           handles.push($(item).attr('href'));
         });
         var content = (b64EncodeUnicode(handles.join('\n')));
-        jQuery('h4.subheading').html('<a id="download-files-list" href="data:application/octet-stream;charset=utf-8;base64,' + content + '">Download files list</a>')
+        jQuery('h4.subheading').html(
+            '<a id="download_files-list" '
+            + 'download="filestack_files_list.txt" '
+            + 'href="data:application/octet-stream;charset=utf-8;base64,' + content
+            + '">' + 'Download files list' + '</a>'
+        );
+        console.log('Done preparing file for download!\n' +
+                    'Now simply click on \'Download files list\' link in the header of the page!');
       }
     }
   };
-
   jQuery(document).ajaxSuccess(loadMoreCallback);
+  console.log('Staring collect your files links...');
   $btnLoadMore.trigger('click', true);
-
 })();
