ccb64ce50ccd12793385a6b250ec2436c2e25da4
Andrey Baryshnikov <andrewshkovskii@gmail.com>
2018-09-24 10:16:38 +0300

    Fix `build_url`

--- migro/filestack/utils.py
+++ migro/filestack/utils.py
@@ -10,7 +10,7 @@
 def build_url(handle_or_url):
     """Builds URL for filestack-hosted file based on file handler or, 
     if it is already URL - return it."""
-    if handle_or_url.startswith('https://'):
+    if handle_or_url.startswith('https://') or handle_or_url.startswith('http://'):
         return handle_or_url
     else:
         return 'https://cdn.filestackcontent.com/{0}'.format(handle_or_url)
