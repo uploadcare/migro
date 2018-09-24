72e8f9a595f935fca5980179f7810488f28f3d09
Andrey Baryshnikov <andrewshkovskii@gmail.com>
2018-09-24 10:24:57 +0300

    Fix classifier

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5.3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ]
 )
