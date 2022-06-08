1e78436740402415bb38eea18c6c48bf1c2db3a3
Dmitry Mukhin <dm@uploadcare.com>
2022-06-08 16:53:22 +0200

    Add User Agent header to upload requests

--- migro/uploader/utils.py
+++ migro/uploader/utils.py
@@ -14,11 +14,8 @@ from aiohttp import ClientSession, TCPConnector
 from asyncio import get_event_loop
 from urllib.parse import urljoin
 
-from migro import settings
+from migro import settings, __version__ as version
 
-from aiohttp import ClientSession, TCPConnector
-
-from migro import settings
 
 loop = get_event_loop()
 session = ClientSession(connector=TCPConnector(verify_ssl=False, loop=loop))
@@ -36,6 +33,12 @@ async def request(path, params=None):
     path = path.lstrip('/')
     url = urljoin(settings.UPLOAD_BASE, path)
 
+    headers = {
+        "User-Agent": f"Migro/{version}/{settings.PUBLIC_KEY}"
+    }
+
+    if params is None:
+        params = {}
     params['pub_key'] = settings.PUBLIC_KEY
     params['UPLOADCARE_PUB_KEY'] = settings.PUBLIC_KEY
 
@@ -47,7 +50,11 @@ async def request(path, params=None):
         params['expire'] = expire_timestamp
 
     response = await session.request(
-        method='get', url=url, allow_redirects=True, params=params)
+        method='get',
+        url=url,
+        headers=headers,
+        allow_redirects=True,
+        params=params)
     return response
 
 
